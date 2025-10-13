48a8591743746f735bb9fe3e4d367cd2b5245fe7
xi-pec <xepicscripter@gmail.com>
2025-10-13 12:22:06 +0800

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,48 @@
+import { SerialPort } from "serialport";
+import { ReadlineParser } from "@serialport/parser-readline"
+import express, { json } from "express"
+import extend from "express-ws"
+
+const { app: server, getWss } = extend(express().use(json()))
+
+let port: SerialPort | null = null
+let parser: ReadlineParser | null = null
+
+server.get("/list", async (req, res) => {
+    const ports = await SerialPort.list()
+    res.send(ports)
+})
+
+server.post("/select", async (req, res) => {
+    if (req.body.path) {
+        port = new SerialPort({ path: req.body.path, baudRate: 9600 })
+        parser = port.pipe(new ReadlineParser({ delimiter: "\r" }))
+
+        parser.on("data", (data) => {
+            getWss().clients.forEach(socket => socket.send(data))
+        })
+
+        port.on("close", () => {
+            port = null
+        })
+
+        port.on("error", () => {
+            port = null
+        })
+
+        parser.on("close", () => {
+            parser = null
+        })
+    } else {
+        port?.destroy()
+        parser?.destroy()
+    }
+
+    res.send("OK")
+})
+
+server.ws("/listen", () => {})
+
+server.listen(4567, () => {
+    console.log(`Listening at port 4567`)
+})
